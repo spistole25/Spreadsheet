01439c5e9ea3eb8d5f99dd60cfc441ad7ef74132
spistole25 <spistole25@gmail.com>
2024-04-20 11:42:18 -0500

    updating js file

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const average = nums => sum(nums) / nums.length;
 
 const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
@@ -27,7 +37,12 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
       container.appendChild(input);
     })
   })
+}
+
+const update = event => {
+
 }
