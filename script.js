30c160bf77b13fed117a6bf9f95bc796ce2fd010
spistole25 <spistole25@gmail.com>
2024-04-29 23:31:20 -0500

    spreadsheet updates .js file

--- script.js
+++ script.js
@@ -23,7 +23,9 @@ const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).
 const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
-
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map();
 }
 
 window.onload = () => {
