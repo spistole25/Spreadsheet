6653b058936fd210e3e82bb3ad8e119e6563d317
spistole25 <spistole25@gmail.com>
2024-05-01 01:19:44 -0500

    oops, one more. I'm tired and making mistakes

--- script.js
+++ script.js
@@ -41,6 +41,23 @@ const spreadsheetFunctions = {
   random: ([x, y]) => Math.floor(Math.random() * y + x),
   range: nums => range(...nums),
   nodupes: nums => [...new Set(nums).values()]
+  const spreadsheetFunctions = {
+  
+  sum,
+  average,
+  median,
+  even: nums => nums.filter(isEven),
+  someeven: nums => nums.some(isEven),
+  everyeven: nums => nums.every(isEven),
+  firsttwo: nums => nums.slice(0, 2),
+  lasttwo: nums => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: nums => range(...nums),
+  nodupes: nums => [...new Set(nums).values()],
+  "": nums => nums
+
 }
 
 const applyFunction = str => {
